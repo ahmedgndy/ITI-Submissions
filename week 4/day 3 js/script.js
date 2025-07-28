3dd41b95f03d8ede9c5c643acd0d52833263a3fe
ahmedgendy <ahmed.mamdouh.elgndy@gmail.com>
2025-07-28 18:16:17 +0300

    js day 2 Task 2

--- week 4/day 3 js/script.js
+++ week 4/day 3 js/script.js
@@ -2,3 +2,12 @@
 
 var number = parseInt(prompt("enter a number to check for positive or negative or zero"));
 alert (number>=0 ? (number>0 ? "positive": "zero " ): "negative")
+
+//task 2
+
+var result = confirm("do you want to proceed? ")
+if (result){
+    alert("You choose to proceed ")
+}else {
+    alert("action Canceled")
+}
