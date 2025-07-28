0cc19fab73b74985511ec44bc41518ee3ba81404
ahmedgendy <ahmed.mamdouh.elgndy@gmail.com>
2025-07-28 18:47:42 +0300

    js day 2 task 7

--- week 4/day 3 js/script.js
+++ week 4/day 3 js/script.js
@@ -1,37 +1,37 @@
 // task  1
 
 var number = parseInt(prompt("enter a number to check for positive or negative or zero"));
-alert (number>=0 ? (number>0 ? "positive": "zero " ): "negative")
+alert(number >= 0 ? (number > 0 ? "positive" : "zero ") : "negative")
 
 //task 2
 
 var result = confirm("do you want to proceed? ")
-if (result){
+if (result) {
     alert("You choose to proceed ")
-}else {
+} else {
     alert("action Canceled")
 }
 
 //task  3
-var num = 10 ;
-console.log(num % 2 ===0 ? "hi " : "helloo");
+var num = 10;
+console.log(num % 2 === 0 ? "hi " : "helloo");
 
 //task 4
 do {
-var age = parseInt(prompt("enter you age"))
-}while(age>0)
+    var age = parseInt(prompt("enter you age"))
+} while (age < 0)
 
-var  cata = "chile"
-switch(age){
+var cata = "chile"
+switch (age) {
     case age <= 10:
-        break ; 
-    case age <=18 :
-        cata  = "teen"
-       break; 
-    case   age <= 50 :
+        break;
+    case age <= 18:
+        cata = "teen"
+        break;
+    case age <= 50:
         cata = "grown"
-        break 
-    default : 
+        break
+    default:
         cata = "old"
 
 }
@@ -48,5 +48,18 @@ console.log("you are  " + cata)
 //task 6 
 
 
-var word = prompt("enter a word") ;
-alert (word[0].toUpperCase + word.slice(1));
+var word = prompt("enter a word");
+alert(word[0].toUpperCase() + word.slice(1));
+//task 7
+
+do {
+    var ChoosenColor;
+    var isFavorite;
+    var ischoosen;
+    do {
+        ChoosenColor = prompt("your favorite color is : ");
+        isFavorite = confirm("is your favorite color " + ChoosenColor)
+
+    } while (isFavorite)
+    isChoosen = confirm("you choose " + ChoosenColor)
+} while (isChoose)
