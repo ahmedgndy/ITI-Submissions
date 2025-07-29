70916e55efad8908a2fc154fa7cb92375566d55e
ahmedgendy <ahmed.mamdouh.elgndy@gmail.com>
2025-07-29 21:02:17 +0300

    learned window Timer

--- week 4/on my own/29July/js-mahara-Window-object/script.js
+++ week 4/on my own/29July/js-mahara-Window-object/script.js
@@ -23,3 +23,40 @@ function moveByCoordinato() {
     win.moveBy(50, 50)
     win.focus()
 }
+
+//Timer methouds
+var timerId ;
+function startAlert() {
+
+   timeId = window.setInterval(function(){
+    alert("alert every 2 second")
+   } , 2000);//functionalty , time in miliseconde 
+}
+
+function stopAlert () {
+    clearInterval(timerId)
+}
+
+function startTimeoutAlert() {
+
+    window.setTimeout(function(){
+    alert("alert onetime on click")
+   } , 2000);//functionalty , time in miliseconde 
+}
+
+
+
+//timeout excused for one time
+//but i can use something like  recursion to make it run forever
+
+function startTimeOutUsingRecursion() {
+    alert("alert onetime on click")
+    //
+    //
+    //
+    timerId = window.setTimeout(startTimeOutUsingRecursion , 2000);
+}
+
+function stoptimeOut() {
+    clearTimeout(timerId)
+}
