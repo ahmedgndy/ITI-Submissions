ffef72b22b130b329bc51a88668b3fe336577e71
ahmedgendy <ahmed.mamdouh.elgndy@gmail.com>
2025-07-30 10:32:40 +0300

    js day 3 task 3

--- week 4/day 4 js/script.js
+++ week 4/day 4 js/script.js
@@ -5,12 +5,40 @@ var userInput
 
 do {
     userInput = prompt("enter a password : ")
-}while(userInput == password )
+} while (userInput == password)
 
 //task 2
 
-var sum =  0 ;
-for(i =  0 ; i <=100 ; i+=2) {
-    sum+=i ;
+var sum = 0;
+for (i = 0; i <= 100; i += 2) {
+    sum += i;
 }
 console.log(sum);
+
+//task 3
+
+function calculate() {
+    var fristNumber = parseFloat(prompt("enter number 1"));
+    var operator = prompt("enter operator");
+    var secondNumber = parseFloat(prompt("enter socend number"));
+    var result = 0;
+
+    switch (operator) {
+        case "+":
+            result = fristNumber + secondNumber;
+            break;
+
+        case "-":
+            result = fristNumber - secondNumber;
+            break;
+        case "/":
+            result = fristNumber / secondNumber;
+
+            break;
+
+        case "%":
+            result = fristNumber / secondNumber;
+            break;
+    }
+    return result ; 
+}
