f5cbc980321bab028d13daa355ea56e885e9d191
ahmedgendy <ahmed.mamdouh.elgndy@gmail.com>
2025-07-30 10:26:22 +0300

    js day 3 task 2

--- week 4/day 4 js/script.js
+++ week 4/day 4 js/script.js
@@ -6,3 +6,11 @@ var userInput
 do {
     userInput = prompt("enter a password : ")
 }while(userInput == password )
+
+//task 2
+
+var sum =  0 ;
+for(i =  0 ; i <=100 ; i+=2) {
+    sum+=i ;
+}
+console.log(sum);
