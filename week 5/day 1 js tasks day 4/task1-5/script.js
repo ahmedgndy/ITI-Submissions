366e8835db80a783888b66e5c5494769bb539296
ahmedgndy <ahmed.mamdouh.elgndy@gmail.com>
2025-08-04 17:42:01 +0300

    js day 4 task 4

--- week 5/day 1 js tasks day 4/task1-5/script.js
+++ week 5/day 1 js tasks day 4/task1-5/script.js
@@ -36,3 +36,22 @@ function swapKeyVaule(obj) {
 }
 const obj = { a: 1, b: 2 };
 console.log(swapKeyVaule(obj))
+
+//Task 4 
+// create a "person" object that includes nested objects 
+//for "address" and "job", and a method to display all details.
+let person = {
+     name: "Ahmed",
+    age: 25,
+    address : {address1 : "Fayoum - new Fayoum" ,
+               address2 : "Fayoum - old Fayoum" },
+    job : {currentJob : ".net Developer"},
+    displayDetails :  function () {
+        console.log(`Name: ${this.name}`);
+        console.log(`Age: ${this.age}`);
+        console.log(`Current Address: ${this.address.address1}`);
+        console.log(`Old Address: ${this.address.address2}`);
+        console.log(`Current Job: ${this.job.currentJob}`);
+        console.log(`Previous Job: ${this.job.previousJob}`);
+    }
+}
