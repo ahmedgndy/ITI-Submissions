f8ab6eeb08c681fa39b9f1baf86e7b96067a948e
ahmedgndy <ahmed.mamdouh.elgndy@gmail.com>
2025-08-12 14:43:37 +0300

    learned about singleton desing pattern

---
+++ week 6/mon day 2 advanced cs/lab03 FTP client Project/FTB.cs
@@ -0,0 +1,43 @@
+﻿using lab03_FTP_client_Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab03_FTP_client_Project
+{
+    enum Type
+    {
+        None = 0,
+        ethernet = 1, 
+        token= 2
+    }
+    internal class  NIC
+    {
+      public string manficature { get; }
+      public string macAddress { get; }
+       
+      public Type type { get; }
+
+
+        private static readonly NIC instance = new NIC(); 
+
+        public static  NIC Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        private  NIC()
+        {
+            manficature = "intel";
+            macAddress = "00-14-22-01-23-45";
+            type = Type.ethernet;
+
+        }
+    }
+}
---
+++ week 6/mon day 2 advanced cs/lab03 FTP client Project/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace lab03_FTP_client_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NIC card = NIC.Instance;
+
+            Console.WriteLine(card.macAddress);
+            Console.WriteLine(card.manficature);
+            Console.WriteLine(card.type);
+           NIC card2 = NIC.Instance;
+
+            Console.WriteLine(card2.GetHashCode());
+            Console.WriteLine(card.GetHashCode());
+        }
+    }
+}
